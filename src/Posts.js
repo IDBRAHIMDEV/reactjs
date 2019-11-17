ec6a5d6f84d3cd907f852c8285b6d219d6ee1c2c
Mohamed <idbrahimdev@gmail.com>
2019-11-17 15:05:55 +0100

    json server

--- src/Posts.js
+++ src/Posts.js
@@ -9,11 +9,14 @@ class Posts extends Component {
      
 
     state = {
+        displayForm: false,
+        title: '',
+        body: '',
         posts: []
     }
 
     getPosts = () => {
-        axios.get('https://jsonplaceholder.typicode.com/posts')
+        axios.get('http://localhost:5000/posts')
              .then((res) => {
                  this.setState({
                      posts: res.data
@@ -23,14 +26,69 @@ class Posts extends Component {
              .catch((err) => console.error(err))
     }
 
+    display = () => {
+        this.setState({
+            displayForm: !this.state.displayForm
+        })
+    }
+
+    getValueInput = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    persistPost = () => {
+
+        let post = {
+            title: this.state.title,
+            body: this.state.body,
+        }
+        axios.post('http://localhost:5000/posts', post)
+             .then(res => {
+                 this.setState({
+                     title: '',
+                     body: '',
+                     displayForm: false
+                 })
+
+                 this.getPosts();
+             })
+             .catch(err => console.error(err))
+    }
+
     render() {
         console.log('render');
         return (
             <div>
-               
-               <div className="text-right">
-                   <button className="btn btn-success btn-sm my-3">+</button>
+
+<div className="text-right">
+                   <button onClick={this.display} className="btn btn-success btn-sm my-3">+</button>
                 </div>
+                 
+                { this.state.displayForm ? (
+                    <div className="row my-3">
+                    <div className="col-6 mx-auto">
+
+                        <div className="form-group">
+                            <label htmlFor="title">Title</label>
+                            <input name='title' onChange={this.getValueInput} id='title' type="text" className="form-control"/>
+                            { this.state.title }
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="body">Body</label>
+                            <textarea name='body' onChange={this.getValueInput} className='form-control' id="body" rows="2"></textarea>
+                            { this.state.body }
+                        </div>
+
+                        <button onClick={this.persistPost} className="btn btn-success btn-block">Add post</button>
+
+                    </div>
+                </div> 
+                ) : null }
+
+                
 
                <table className="table table-striped">
                    <thead>
